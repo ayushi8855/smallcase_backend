f09d7ea6f041fa25375a37c8d1ea4cbc4e212d00
Ayushi Jain <97507160+ayushi8855@users.noreply.github.com>
2025-04-12 16:58:46 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = ()=>{
-    return mongoose.connect("mongodb+srv://smallcase:small@cluster0.tw22m.mongodb.net/smallcase?retryWrites=true&w=majority");
+    return mongoose.connect("mongodb+srv://ayushi:ayushi123@cluster0.wzplxy8.mongodb.net/smallcase?retryWrites=true&w=majority");
 }
